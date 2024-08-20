804de74623ff3ac279e0624ff0b4deab47d402a9
alexdja <alexdja202@gmail.com>
2024-08-21 01:00:35 +0400

    add|edit|delete functions realization was added

--- skud_api.py
+++ skud_api.py
@@ -1,6 +1,6 @@
 import json
 from typing import Any
-import requests
+import requests as req
 from requests.auth import AuthBase
 
 from singleton import Singleton
@@ -14,60 +14,85 @@ class TokenAuth(AuthBase):
 
     def __call__(self, req):
         # изменяем и возвращаем запрос
-        req.headers['X-Id'] = self.id
-        req.headers['X-Auth'] = self.token
+        if self.token:
+            req.headers['X-Id'] = self.id
+            req.headers['X-Auth'] = self.token
         return req
 
 class SkudApiRequsts(Singleton):
-    
-    def __init__(self, url: str, id: int) -> None:
+    def __init__(self, url: str, id) -> None:
         self.url = url
-        self.token = None
         self.id = id
+        self.token_auth = None
+        self.requests = { "get"   : req.get, 
+                          "post"  : req.post,
+                          "put"   : req.put,
+                          "delete": req.delete }
 
-    def get(self, body: Any, path="") -> requests.Response | None:
+    def request(self, type: str, body: Any, path="") -> req.Response | None:
         try:
-            auth = TokenAuth(self.token, self.id) if self.token else None
-
-            response = requests.get(self.url+path, data=body, auth=auth)
+            response = self.requests[type](self.url+path, data=body, auth=self.token_auth)
             if response.status_code == 200:
                 return response
             else:
                 print(f"Ошибка {response.status_code}: {response.reason}")
-        except BaseException as error:
+        except Exception as error:
             print("get ERR", error)
 
-    def fmt(self, action, data) -> dict:
-        return {"action": action, 
-                "data"  : json.dumps(data)}
+    def fmt(self, data) -> dict:
+        return { "data"  : json.dumps(data) }
     
-    def get_table(self, table: str, start: int, sorting_rules: dict) -> dict | None:
+    def get_table(self, table: str, start: int, order_column: str, order_type: bool) -> dict | None:
         # start = -1 означает, что требуется предоставить все записи
         # -----------------------------Для тестов-----------------------------
         return {'data': [], 'error': ''}
         # -----------------------------Для тестов-----------------------------
 
-        action = f"{table} query"
         data = {"start"       : start, 
-                "sorting_rules"  : sorting_rules}
+                "order_type"  : order_type, 
+                "order_column": order_column}
  
-        response = self.get(self.fmt(action,  data), "/ui")
+        response = self.get(self.fmt(data), f"/ui/{table}")
         try: 
             return response.json()
+        except Exception as error:
+            print("response:", response.text if response else "None", 
+                  "ERR:", error)
+            
+    def get_rights(self, getall=False):
+        return self.get(self.fmt({"all": getall}), "/ui/rights")
+    
+    def authentication(self, key: int) -> tuple[bool, str]:
+        data = json.dumps({"key": key, "id": self.id})
+        response = self.get({"auth": data}, "/auth")
+        try:
+            answer = response.json()
+            print(answer)
+
+            err = "answer is None"
+            if answer:
+                err = answer["error"]
+                if err == "":
+                    self.token_auth = TokenAuth(answer["data"], self.id)
+                    return True, err
+            return False, err
         except BaseException as error:
-            print("response:", response.text if response else "None", "ERR:", error)
-        
-    def add_order(self, table: str, data: dict) -> bool:
-        pass
+            return False, error
+            
+    def add_row(self, table: str, values: dict) -> bool:
+        return self.request("post", self.fmt(values), f"/ui/{table}")
 
     def check_order(self, table: str, data: dict):
+        # AUTOMATA : Зачем эта штука нужна ??
         pass
 
-    def edit_order(self, table: str, id, new_data: dict):
-        pass
+    def edit_order(self, table: str, id: Any, new_values: dict):
+        data = self.fmt({"key": id, "values": new_values})
+        return self.request("post", data, f"/ui/{table}")
 
     def delete_order(self, table: str, id):
-        pass
+        data = self.fmt({"key": id})
+        return self.request("delete", data, f"/ui/{table}")
 
     def authentication(self, key: int) -> tuple[bool, str]:
         data = json.dumps({"key": key, "id": self.id})
